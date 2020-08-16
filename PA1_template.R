7b1f51da8139cba122c5efb6a5693365f72f25a3
Anubhav <65869678+anubhav255@users.noreply.github.com>
2020-08-16 22:00:34 +0530

    Update PA1_template.R

--- PA1_template.R
+++ PA1_template.R
@@ -1,73 +1,57 @@
+install.packages("timeDate")
+library(timeDate)
+library(ggplot2)
 
-## ----loaddata------------------------------------------------------------
+## ----leer data de archivo local en mi folder------------------------------------------------------------
+fileurl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
+download.file( fileurl, "activity.zip")  
 unzip(zipfile="activity.zip")
-data <- read.csv("activity.csv")
+
+activity <- read.csv("activity.csv")
+
+
+## ----numero de pasos medio tomado por día---------------------------------------------------
+#cuento total de steps mediante sum y los calculo por date, remuevo NA
+steps.date <- aggregate(steps ~ date, data = activity, FUN = sum)
+barplot(steps.date$steps, names.arg = steps.date$date, xlab = "date", ylab = "total steps",main = "Number of steps by date.")
+
+mean(steps.date$steps, na.rm=TRUE)
+median(steps.date$steps, na.rm=TRUE)
 
 
 ## ------------------------------------------------------------------------
-library(ggplot2)
-total.steps <- tapply(data$steps, data$date, FUN=sum, na.rm=TRUE)
-qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
-mean(total.steps, na.rm=TRUE)
-median(total.steps, na.rm=TRUE)
+steps.interval <- aggregate(steps ~ interval, data = activity, FUN = mean)
+plot(steps.interval, type = "l")
 
 
 ## ------------------------------------------------------------------------
-library(ggplot2)
-averages <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
-                      FUN=mean, na.rm=TRUE)
-ggplot(data=averages, aes(x=interval, y=steps)) +
-        geom_line() +
-        xlab("5-minute interval") +
-        ylab("average number of steps taken")
-
-
-## ------------------------------------------------------------------------
-averages[which.max(averages$steps),]
+steps.interval$interval[which.max(steps.interval$steps)]
 
 
 ## ----how_many_missing----------------------------------------------------
-missing <- is.na(data$steps)
-# How many missing
-table(missing)
-
+sum(is.na(activity))
 
 ## ------------------------------------------------------------------------
-# Replace each missing value with the mean value of its 5-minute interval
-fill.value <- function(steps, interval) {
-        filled <- NA
-        if (!is.na(steps))
-                filled <- c(steps)
-        else
-                filled <- (averages[averages$interval==interval, "steps"])
-        return(filled)
-}
-filled.data <- data
-filled.data$steps <- mapply(fill.value, filled.data$steps, filled.data$interval)
 
+
+activity.filled <- merge(activity, steps.interval, by = "interval", suffixes = c("", ".y"))
+nas <- is.na(activity.filled$steps)
+activity.filled$steps[nas] <- activity.filled$steps.y[nas]
+activity.filled <- activity.filled[, c(1:3)]
+
+sum(is.na(activity.filled))
 
 ## ------------------------------------------------------------------------
-total.steps <- tapply(filled.data$steps, filled.data$date, FUN=sum)
-qplot(total.steps, binwidth=1000, xlab="total number of steps taken each day")
-mean(total.steps)
-median(total.steps)
+steps.date.filled <- aggregate(steps ~ date, data = activity.filled, FUN = sum)
+barplot(steps.date.filled$steps, names.arg = steps.date.filled$date, xlab = "date", ylab = "total steps",main = "Number of steps by date.")
+mean(steps.date.filled$steps, na.rm=TRUE)
+median(steps.date.filled$steps, na.rm=TRUE)
 
 
-## ------------------------------------------------------------------------
-weekday.or.weekend <- function(date) {
-        day <- weekdays(date)
-        if (day %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
-                return("weekday")
-        else if (day %in% c("Saturday", "Sunday"))
-                return("weekend")
-        else
-                stop("invalid date")
-}
-filled.data$date <- as.Date(filled.data$date)
-filled.data$day <- sapply(filled.data$date, FUN=weekday.or.weekend)
 
+weeked <- ifelse(isWeekend((activity.filled$date)), "weekend", "weekday")
+final <- data.frame(activity.filled,weeked)
+average_steps <- aggregate(steps ~ weeked + interval, data = final,mean)
 
-## ------------------------------------------------------------------------
-averages <- aggregate(steps ~ interval + day, data=filled.data, mean)
-ggplot(averages, aes(interval, steps)) + geom_line() + facet_grid(day ~ .) +
-        xlab("5-minute interval") + ylab("Number of steps")
+qplot(interval, steps, data = average_steps, facets = weeked~., geom = "line")
+qplot(interval, steps, data = average_steps, color = weeked, geom = "line")
